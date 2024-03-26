a84b992eb959c601c95ce68a5e9a70633ac12798
chnlr <162039036+chnlr@users.noreply.github.com>
2024-03-26 15:32:33 -0400

    Update app.js

--- app.js
+++ app.js
@@ -33,6 +33,34 @@ function playExplodeAnimation() {
         window.location.href = 'http://chahanler.com/secret';
     }, 1000); // Adjust the delay time if needed
 }
+// Get a reference to the "Hello" section
+const helloSection = document.getElementById('hello-message');
+
+// Options for the Intersection Observer
+const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.5 // Trigger when 50% of the element is visible
+};
+
+// Callback function to handle intersection changes
+const intersectionCallback = (entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            // If the section is intersecting with the viewport, remove the hidden class
+            helloSection.classList.remove('hidden');
+        } else {
+            // If the section is not intersecting with the viewport, add the hidden class
+            helloSection.classList.add('hidden');
+        }
+    });
+};
+
+// Create a new Intersection Observer
+const observer = new IntersectionObserver(intersectionCallback, options);
+
+// Start observing the "Hello" section
+observer.observe(helloSection);
 
 // Add event listener to dividerImage to trigger explode animation on click
 dividerImage.addEventListener('click', () => {
