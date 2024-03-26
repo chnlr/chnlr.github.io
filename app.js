eae208ffa13d247d3fba6ac425641452d6931c2d
chnlr <162039036+chnlr@users.noreply.github.com>
2024-03-26 06:48:00 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,14 +3,6 @@ const depopsImage = document.getElementById('depops-image');
 const tiktokImage = document.getElementById('tiktok-image');
 const dividerImage = document.getElementById('divider-image');
 
-// Function to preload images
-const preloadImages = (urls) => {
-  urls.forEach((url) => {
-    const img = new Image();
-    img.src = url;
-  });
-};
-
 // Function to change the image source to the animated GIF
 function playGif(element, divider) {
     if (element === 'depops') {
@@ -49,28 +41,3 @@ dividerImage.addEventListener('click', () => {
     clickSound.play();
     playExplodeAnimation();
 });
-
-// Add scroll event listener to fade out content when scrolling down
-window.addEventListener('scroll', () => {
-    const helloMessage = document.getElementById('hello-message');
-    const links = document.getElementById('links');
-    const scrollPosition = window.scrollY;
-
-    // Calculate the distance from the top of the page to the "Hello" message
-    const helloMessageOffsetTop = helloMessage.offsetTop;
-
-    // Check if the scroll position is greater than or equal to the offset of the "Hello" message
-    if (scrollPosition >= helloMessageOffsetTop) {
-        // Add class to fade out the links
-        links.classList.add('fade-out-hide');
-    } else {
-        // Remove class to show the links
-        links.classList.remove('fade-out-hide');
-    }
-});
-
-// URLs of the GIFs to preload
-const gifUrls = ['depops wiggle.gif', 'tiktok wiggle.gif'];
-
-// Preload GIFs
-preloadImages(gifUrls);
