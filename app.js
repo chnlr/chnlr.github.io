69bc27c5a649df229ab33bd63ba2f112780f6656
chnlr <162039036+chnlr@users.noreply.github.com>
2024-03-27 10:29:50 -0500

    Update app.js

--- app.js
+++ app.js
@@ -36,32 +36,7 @@ function playExplodeAnimation() {
     }, 1000); // Adjust the delay time if needed
 }
 
-// Options for the Intersection Observer
-const options = {
-    root: null,
-    rootMargin: '0px',
-    threshold: 1.0 // Trigger when the entire element is visible
-};
 
-// Callback function to handle intersection changes
-const intersectionCallback = (entries, observer) => {
-    entries.forEach(entry => {
-        console.log(entry); // Log the entry object
-        if (entry.isIntersecting) {
-            // If the section is intersecting with the viewport, remove the hidden class
-            helloSection.classList.remove('hidden');
-        } else {
-            // If the section is not intersecting with the viewport, add the hidden class
-            helloSection.classList.add('hidden');
-        }
-    });
-};
-
-// Create a new Intersection Observer
-const observer = new IntersectionObserver(intersectionCallback, options);
-
-// Start observing the "Hello" section
-observer.observe(helloSection);
 
 // Add event listeners to TikTok and Depop images
 depopsImage.addEventListener('mouseover', () => playGif('depops'));
