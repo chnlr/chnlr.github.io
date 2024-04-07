136a995fd3d01c2a37547d8ece2afbe8a8db2257
chnlr <162039036+chnlr@users.noreply.github.com>
2024-04-07 12:00:38 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,25 +2,18 @@ document.addEventListener('DOMContentLoaded', function () {
     // Get references to HTML elements
     const depopsImage = document.getElementById('depops-image');
     const tiktokImage = document.getElementById('tiktok-image');
-    const dividerImage = document.getElementById('divider-image');
-    let dividerRotated = false; // Variable to track if divider has been rotated
+    // No need to get reference to divider image since we're not rotating it anymore
 
-    // Function to change the image source to the animated GIF and rotate divider image
+    // Function to change the image source to the animated GIF
     function playGif(element) {
         if (element.id === 'depops-image') {
             element.src = 'depopwiggle.gif'; // Update the source of the element passed as parameter
         } else if (element.id === 'tiktok-image') {
             element.src = 'tiktokwiggle.gif'; // Update the source of the element passed as parameter
         }
-
-        // Rotate the divider GIF only if it hasn't been rotated yet
-        if (!dividerRotated) {
-            dividerImage.style.transform = 'rotate(180deg)'; // Rotate the divider GIF
-            dividerRotated = true; // Set the flag to indicate divider has been rotated
-        }
     }
 
-    // Function to change the image source back to the static image and reset divider rotation
+    // Function to change the image source back to the static image
     function stopGif(element) {
         if (element.id === 'depops-image') {
             element.src = 'depopstill.png'; // Update the source of the element passed as parameter
