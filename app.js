3e89e1c4890410419717f54ce5ae663bfa00b224
pt <162039036+chnlr@users.noreply.github.com>
2024-05-22 18:57:24 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener('DOMContentLoaded', function() {
     setTimeout(function() {
-        document.getElementById("sidebar").style.left = "0";
-        setTimeout(function() {
-            document.querySelector(".sidebar a").classList.add("visible");
-        }, 500); // Fade in the link after the sidebar has slid in
-    }, 3000);
+        document.getElementById('sidebar').style.left = '0px'; // Slide in sidebar
+    }, 500); // Delay can be adjusted or removed as per design needs
 });
