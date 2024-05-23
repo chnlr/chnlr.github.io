b9dc52e5ba6d3b4ea96a385a7fd5fbb96bc6fd24
pt <162039036+chnlr@users.noreply.github.com>
2024-05-23 06:37:04 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var sidebar = document.getElementById('sidebar');
-    setTimeout(function() {
-        sidebar.style.top = '0px'; // Slide down the sidebar
-    }, 500);
-
-    var depopImage = document.getElementById('depopImage');
-    depopImage.addEventListener('click', function() {
-        depopImage.src = 'clickdepop.gif';
-        setTimeout(function() {
-            window.location.href = 'https://www.depop.com/chanlr/';
-        }, 1400);
-    });
-});
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>chahanler（ꉺ౪ꉺ）</title>
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div id="sidebar" class="sidebar">
+        <a href="about.html" class="reenie-beanie-regular">About</a>
+        <a href="services.html" class="reenie-beanie-regular">Services</a>
+    </div>
+    <div class="container">
+        <h1 id="title">CHAHANLER</h1>
+        <img src="depop.png" alt="Depop Image" id="depopImage">
+    </div>    
+    <script src="app.js"></script>
+</body>
+</html>
