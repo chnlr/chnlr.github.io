45ccfe97a0901ceb3ed2367c0d16ba3b1a709381
pt <162039036+chnlr@users.noreply.github.com>
2024-05-23 12:13:05 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,26 +1,27 @@
 document.addEventListener('DOMContentLoaded', function() {
     var topMenu = document.querySelector('.top-menu');
-    topMenu.style.top = '-60px'; // Start hidden
+    topMenu.style.top = '-60px';  // Start the menu slightly above the top to be fully hidden
     setTimeout(function() {
-        topMenu.style.top = '0'; // Slide down
+        topMenu.style.top = '0px'; // Slide down to show
     }, 100);
 
     var modal = document.getElementById("myModal");
     var modalImg = document.getElementById("modalImage");
-    var captionText = document.getElementById("caption");
     var images = document.querySelectorAll('.asset-gallery img');
+    var body = document.querySelector('body');
 
     images.forEach(img => {
         img.onclick = function() {
-            modal.style.display = "block";
+            modal.style.display = "flex";
             modalImg.src = this.src;
-            captionText.innerHTML = this.alt;
+            body.classList.add('blur-active'); // Add class to activate blur
         }
     });
 
     var closeBtn = document.querySelector('.close');
     closeBtn.onclick = function() {
         modal.style.display = "none";
+        body.classList.remove('blur-active'); // Remove class to deactivate blur
     }
 
     // Close modal on pressing ESC
@@ -28,6 +29,7 @@ document.addEventListener('DOMContentLoaded', function() {
         evt = evt || window.event;
         if (evt.keyCode == 27) {
             modal.style.display = "none";
+            body.classList.remove('blur-active'); // Remove class to deactivate blur
         }
     };
 });
