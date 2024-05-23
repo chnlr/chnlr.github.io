25faa03f5f23c470029f3169b01fe9670597814d
pt <162039036+chnlr@users.noreply.github.com>
2024-05-22 22:08:04 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // Sidebar slide-in animation
+    var sidebar = document.getElementById('sidebar');
+    setTimeout(function() {
+        sidebar.style.left = '0px'; // Slide in the sidebar
+    }, 500); // Delay can be adjusted as needed
+
+    // Depop image click functionality
     var depopImage = document.getElementById('depopImage');
     depopImage.addEventListener('click', function() {
         // Change to GIF on click and set a timeout to redirect
         depopImage.src = 'clickdepop.gif';
         setTimeout(function() {
             window.location.href = 'https://www.depop.com/chanlr/';
-        }, 1400); // Delay for 1 second before redirecting
+        }, 1400); // Delay for 1.4 seconds before redirecting
     });
 });
